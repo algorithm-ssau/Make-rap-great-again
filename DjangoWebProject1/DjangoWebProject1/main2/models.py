ca134612ca2f8eba547250f742bd060c3a89d500
MaximShch98 <zmakc-98@mail.ru>
2019-05-10 21:43:31 +0400

    Создание моделей
    Создание моделей для бд (тестовая версия)

--- DjangoWebProject1/DjangoWebProject1/main2/models.py
+++ DjangoWebProject1/DjangoWebProject1/main2/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Rapper(models.Model): 
+ id_rapper = models.UUIDField(primary_key=True) 
+ name = models.CharField(max_length=100) 
+ biography = models.CharField(max_length=500) 
+ photo = models.BinaryField(null=True) 
+
+class Season(models.Model): 
+ id_season = models.UUIDField(primary_key=True) 
+ name = models.CharField(max_length=100) 
+ year = models.IntegerField() 
+ text = models.CharField(max_length=500) 
+ photo = models.BinaryField(null=True) 
+ grid = models.BinaryField(null=True)
+
+class Punch(models.Model): 
+ id_punch = models.UUIDField(primary_key=True) 
+ rapper = models.ForeignKey(Rapper) 
+ rapper2 = models.ForeignKey(Rapper,null=True) 
+ season = models.ForeignKey(null=True) 
+ censure = models.IntegerField() 
+ text = models.CharField(max_length=500) 
