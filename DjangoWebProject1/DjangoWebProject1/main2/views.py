52eec3d40e3f6ea9f087c821e05d68b0388ef859
MaximShch98 <zmakc-98@mail.ru>
2019-05-18 18:44:54 +0400

    Работа с БД. Сезоны
    Автоматический вывод сезонов из БД.

--- DjangoWebProject1/DjangoWebProject1/main2/views.py
+++ DjangoWebProject1/DjangoWebProject1/main2/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render 
 from django.http import HttpResponse 
 from django.http import HttpResponseRedirect 
-from .models import Rapper 
+from .models import Season 
 
 
 #from .forms import SubscriberForm 
 # Create your views here. 
 
-def home2(request): 
-    people = Rapper.objects.all() 
-    return render(request, 'main2/index.html',  {"people": people}) 
+#def home2(request): 
+#    seasons = Season.objects.all() 
+#    return render(request, 'main2/index.html',  {"seasons": seasons}) 
 
 
+def home2(request):
+    seasons = Season.objects.all() 
+    return render(request, 'main2/home2.html',  {"seasons": seasons}) 
