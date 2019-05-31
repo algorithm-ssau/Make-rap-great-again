fb45041d4c922b6cb1795af1d0db9c0b27c024da
MaximShch98 <zmakc-98@mail.ru>
2019-05-31 15:44:24 +0400

    Рэперы вывод. Кнопки.
    На каждой своей странице у рэперов отображается ИХ информациия, полученная из БД.
    Добавлена возможность открытия рэперов через кнопки с главной страницы.

--- DjangoWebProject1/DjangoWebProject1/main/urls.py
+++ DjangoWebProject1/DjangoWebProject1/main/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     # Examples:
     # url(r'^$', app.views.home, name='home'),
      url(r'^$', views.home, name='home'),
-     url(r'^rapper$', views.rapper, name='rapper'),
      url(r'^rapper/(?P<rapper_id>\w+)/$', views.rapper1, name='rapper1'), 
 ]
